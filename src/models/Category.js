b541fc26dc27233d5d82ae3959d6f9782f8d5707
chientran148411-beep <chientran148411@gmail.com>
2026-05-16 16:30:48 +0700

    Simplify Category schema field definitions
    Refactor category schema by simplifying field definitions.

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,20 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
 
-  name: {
-    type: String,
-    required: true
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  name: String
 
 });
 
-module.exports = mongoose.model(
-  "Category",
-  categorySchema
-);
+module.exports = mongoose.model("Category", categorySchema);
