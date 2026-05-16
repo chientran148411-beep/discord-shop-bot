92d845ccf11897ffe0b9105d4423bf9e872b19b6
chientran148411-beep <chientran148411@gmail.com>
2026-05-16 20:35:24 +0700

    Create Order class for managing orders

---
+++ src/models/Order.js
@@ -0,0 +1,17 @@
+class Order {
+
+  constructor(
+    id,
+    userId,
+    product
+  ) {
+
+    this.id = id;
+    this.userId = userId;
+    this.product = product;
+
+  }
+
+}
+
+module.exports = Order;
