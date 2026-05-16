15861ca9387e52f347f2429000fd00df5e99463d
chientran148411-beep <chientran148411@gmail.com>
2026-05-16 20:34:20 +0700

    Create Product class with constructor

---
+++ src/models/Product.js
@@ -0,0 +1,21 @@
+class Product {
+
+  constructor(
+    id,
+    category,
+    name,
+    price,
+    desc
+  ) {
+
+    this.id = id;
+    this.category = category;
+    this.name = name;
+    this.price = price;
+    this.desc = desc;
+
+  }
+
+}
+
+module.exports = Product;
